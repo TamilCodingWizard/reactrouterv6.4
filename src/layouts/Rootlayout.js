373635b94af7d1a56d95bfedc49ee60b2947fb02
tamilCodingWizard <tamilcodingwizard@gmail.com>
2023-02-02 15:27:30 +0000

    video09

--- src/layouts/Rootlayout.js
+++ src/layouts/Rootlayout.js
@@ -2,11 +2,15 @@ import { Container } from '@mui/material'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import AppBreadcrumbs from './../components/AppBreadcrumbs';
 
 const Rootlayout = () => {
   return (
     <>
         <Navbar/>
+        <Container sx={{p:3}}>
+            <AppBreadcrumbs/>
+        </Container>
         <Container sx={{p:5}}>
             <Outlet/>
         </Container>
